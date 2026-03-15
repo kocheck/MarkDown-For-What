12e2dc3b430166d2215f74cc4a89d1cb130d357c
Kyle Kochanek <the_designer@kylekochanek.com>
2026-03-15 11:17:19 -0400

    feat: register all new mfw-* components in mfw-index.ts

--- figma-markdown-sync/src/components/mfw-index.ts
+++ figma-markdown-sync/src/components/mfw-index.ts
@@ -4,3 +4,12 @@
 import './mfw-button';
 import './mfw-status';
 import './mfw-drop-zone';
+import './mfw-tab-bar';
+import './mfw-loader';
+import './mfw-theme-selector';
+import './mfw-bottom-bar';
+import './mfw-settings-section';
+import './mfw-settings-row';
+import './mfw-color-input';
+import './mfw-paste-section';
+import './mfw-file-list';
